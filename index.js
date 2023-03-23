971c87f0db9011df8ce3aae9a1e8c6296746967f
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-23 13:43:38 -0400

    rendering reactDOM

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+
+// renders something on the screene
+// what should I place it on the screen and where should I put it. 
 ReactDOM.render(<h1>Hello, everyone!</h1>, 
 document.getElementById("root"))
