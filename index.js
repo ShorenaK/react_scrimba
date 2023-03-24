b4c3827acf0f03049a66e82ccb5c7af9f96f1292
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-24 16:54:08 -0400

    class has been added on nav

--- index.js
+++ index.js
@@ -21,28 +21,52 @@
 //     nav, document.getElementById("root")
 // )
 
-
-
-function Page() {
+function Header() {
     return (
-        <div> 
         <header>
             <nav>
-                <img src="./react-logo.png" width="20px"/ > 
+                <img src="./react-logo.png" width="40px" />
+                <ul className="nav-items"> 
+                <li>Price</li> 
+                <li>About</li>
+                <li>Contact</li>
+                </ul>
             </nav>
         </header>
-        <h1> Reasons
-  I'm excited to learn React </h1>
-        <ol>
-            <li>It's a popular library, so I'll be 
-            able to fit in with the cool kids!</li>
-            <li>I'm more likely to get a job as a developer
-            if I know React</li>
-        </ol>
-        <footer> <small>© 20xx last name development. All rights reserved.</small>  </footer>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
         </div>
     )
 }
 
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
 
-ReactDOM.render( Page, document.getElementById("root"))
+ReactDOM.render(Page , document.getElementById("root"))
+
